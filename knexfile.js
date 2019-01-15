cf97c10b6b3ab40dd2ff948c5722d1f4b8337b6d
kevinmalats <ken_1721@hotmail.es>
2019-01-14 21:43:03 -0500

    j

--- knexfile.js
+++ knexfile.js
@@ -8,36 +8,9 @@ module.exports = {
     connection: 'postgress://postgres:postgres@localhost:5432/test'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'test',
-      user:     'postgres',
-      password: 'postgres'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+      client: "postgresql",
+      connection: process.env.DATABASE_URL + '?ssl=true'
   }
-
 };
